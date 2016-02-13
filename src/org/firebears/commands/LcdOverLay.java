9afd9eea83875e1aa18fa36576ff75403bf2488e
Tyler <Tylerhockeyguy11@gmail.com>
2016-02-12 19:31:43 -0600

    Added LcdOverLay to commands. Refined SelectAuto. Added LcdOverLay to Robot to run in disabled

--- src/org/firebears/commands/LcdOverLay.java
+++ src/org/firebears/commands/LcdOverLay.java
@@ -3,62 +3,41 @@ package org.firebears.commands;
 import org.firebears.RobotMap;
 import org.firebears.util.LiquidCrystal;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.can.CANJNI;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class SelectAuto extends Command {
-	int x = 0;
+public class LcdOverLay extends Command {
+	DriverStation driverStation;
 
-    public SelectAuto() {
+    public LcdOverLay() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	driverStation = DriverStation.getInstance();
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     public void execute() {
+    	
+    	
     	LiquidCrystal lcd = RobotMap.lcd;
-    	x = x + 1;
-    	
-    	
-    	if(x > 3){
-			x = 0;
-	}
-		if(x == 0){
-			lcd.setCursor(10, 0);
-			lcd.print("Hello");
-			lcd.print("     ");
-	}
-		if(x == 1){
-			lcd.setCursor(10, 0);
-			lcd.print("Hi");
-			lcd.print("        ");
-	}
-		if(x == 2){
-			lcd.setCursor(10, 0);
-			lcd.print("Greetings");
-	}
-		if(x == 3){
-			lcd.setCursor(10, 0);
-			lcd.print("Welcome");
-			lcd.print("   ");
-	}
-		
-		lcd.home();
-    	lcd.setCursor(0, 0);
-    	lcd.print("Auto Mode:");
-	lcd.setCursor(0, 2);
-	lcd.print("" + x);
+    	lcd.setCursor(14, 3);
+    	lcd.print(String.format("%6.2f",driverStation.getBatteryVoltage()));
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
