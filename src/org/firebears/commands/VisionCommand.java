af6e5a634a6d32067430fc56e6ad5e3dbb015140
jeron <snowtop105@gmail.com>
2016-02-20 13:17:19 -0600

    Edited vision stuff.

--- src/org/firebears/commands/VisionCommand.java
+++ src/org/firebears/commands/VisionCommand.java
@@ -1,64 +1,52 @@
 package org.firebears.commands;
 
-import java.net.*;
-
 import org.firebears.Robot;
+import org.firebears.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTablesJNI;
 
 /**
- * Command to get vision processing information from the raspberry pi.
+ * Command to process vision processing information from the raspberry pi.
+ * - Aligns the robot to the U shaped target in order to shoot.
  */
 public class VisionCommand extends Command {
-	
-	DatagramSocket dsocket;
+
+	final String NT_CALIBRATION = "vision/calibrationMode";
+	final String NT_DISTANCE = "vision/distance";
+	final String NT_ANGLE = "vision/angle";
+	final String NT_FPS = "vision/fps";
+	final String NT_IMG = "vision/image";
 
     public VisionCommand() {
+    	RobotMap.getPreferencesDouble("vision.hue.lo", 20);
+    	RobotMap.getPreferencesDouble("vision.hue.hi", 40);
+    	RobotMap.getPreferencesDouble("vision.sat.lo", 200);
+    	RobotMap.getPreferencesDouble("vision.sat.hi", 255);
+    	RobotMap.getPreferencesDouble("vision.val.lo", 90);
+    	RobotMap.getPreferencesDouble("vision.val.hi", 180);
         requires(Robot.vision);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	try {
-    		dsocket = new DatagramSocket();
-    	}catch (Exception e) {
-            System.err.println(e);
-        }
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(NetworkTablesJNI.getBoolean("calibrationMode")) {
-    		// Turn the value into a video stream.
-			try {
-				String host = "roborio-2846-frc.local";
-				int port = 554;
-				
-				byte[] message = NetworkTablesJNI.getRaw("Image");
-				
-				// Get the Internet address of the specified host
-				InetAddress address = InetAddress.getByName(host);
-				
-				// Initialize a packet with data and address & send it.
-				DatagramPacket packet = new DatagramPacket(message, message.length, address, port);
-				dsocket.send(packet);
-			} catch (Exception e) {
-				System.err.println(e);
-			}
-    	}else{
-    		
-    	}
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+    	System.out.println("movex: " + NetworkTablesJNI.getDouble(NT_ANGLE) + ",disty: "
+    			+ NetworkTablesJNI.getDouble(NT_DISTANCE));
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        dsocket.close();
     }
 
     // Called when another command which requires one or more of the same
@@ -66,4 +54,5 @@ public class VisionCommand extends Command {
     protected void interrupted() {
     	end();
     }
+
 }
