d3c427af14e7e31ab8610633df2f1841c54bc184
keith <KeithRieck@gmail.com>
2016-04-18 18:46:17 -0500

    Update to Vision aim command.

--- src/org/firebears/commands/VisionAimCommand.java
+++ src/org/firebears/commands/VisionAimCommand.java
@@ -10,64 +10,77 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class VisionAimCommand extends Command {
 
-	public double AngleTolerance = .1;
-	private boolean goBack;
+	final public double AngleTolerance = .1;
+	final public double DistanceTolerance = 5;
+	final private boolean goBack;
 	final static double TARGET_DISTANCE = -65.;
 	final static double FORWARD_SPEED = .6;
 	final static double TURN_SPEED = .5;
-	
-    public VisionAimCommand(boolean backward) {
-    	requires(Robot.chassis);
-    	requires(Robot.vision);
-    	goBack = backward;
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public VisionAimCommand(boolean backward) {
+		requires(Robot.chassis);
+		requires(Robot.vision);
+		goBack = backward;
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	SmartDashboard.putNumber("Angle", Robot.vision.getAngle());
-    	SmartDashboard.putNumber("Distance Away", Robot.vision.getRemainingDistance());
-    	Robot.chassis.drive((goBack ? -2: 1) * (.4 * Robot.vision.getAngle()) +
-    			(.6 * (Robot.vision.getAngle() > 0 ? TURN_SPEED : -TURN_SPEED)),
-    			goBack ? FORWARD_SPEED : -FORWARD_SPEED);
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-    	double angle = Robot.vision.getAngle();
-    	double dist = Robot.vision.getRemainingDistance();
-    	
-    	System.out.println("Dist = " + dist + "/ " + TARGET_DISTANCE + ", Angle = " + angle);
-    	// If Remaining Distance is within tolerance, or past, then stop.
-    	if((dist <= TARGET_DISTANCE) && goBack == false) {
-        	System.out.println("Reached Target: Dist = " + dist + ", Angle = " + angle);
-    		return true;
-    	}else{
-    		return false;
-    	}
-/*    	if(angle > -AngleTolerance && angle < AngleTolerance) {
-    		// Angle is in the acceptable range.
-    		if(dist > -DistanceTolerance && dist < DistanceTolerance) {
-    			// Distance is in the acceptable range.
-    			return true;
-    		}else{
-    			return false;
-    		}
-    	}else{
-    		// Angle needs to be adjusted
-    		return false;
-    	}*/
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		double angle = Robot.vision.getAngle();
+		double dist = Robot.vision.getRemainingDistance();
+		SmartDashboard.putNumber("Angle", angle);
+		SmartDashboard.putNumber("Distance Away", dist);
+		double rotateValue = (goBack ? -2 : 1) * (.4 * angle) + (.6 * (angle > 0 ? TURN_SPEED : -TURN_SPEED));
+		double moveValue = goBack ? FORWARD_SPEED : -FORWARD_SPEED;
+		Robot.chassis.drive(rotateValue, moveValue);
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		double angle = Robot.vision.getAngle();
+		double dist = Robot.vision.getRemainingDistance();
+		System.out.printf("Dist = %8.1f / %8.1f, Angle = %8.4f   : \n)", dist, TARGET_DISTANCE, angle);
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+		// If going forwards and remaining distance is within tolerance, or past, then stop.
+		if ((goBack == false) && (dist <= TARGET_DISTANCE) ) {
+			System.out.println("Reached Target: Dist = " + dist + ", Angle = " + angle);
+			return true;
+		}
+		
+		// If both distance and angle are within tolerance, then stop
+		if ((Math.abs(dist - TARGET_DISTANCE) <= DistanceTolerance) 
+				&& (Math.abs(angle) <= AngleTolerance)) {
+			return true;
+		}
+		
+		return false;
+
+		// if (angle > -AngleTolerance && angle < AngleTolerance) {
+		// // Angle is in the acceptable range.
+		// if (dist > -DistanceTolerance && dist < DistanceTolerance) {
+		// // Distance is in the acceptable range.
+		// return true;
+		// } else {
+		// return false;
+		// }
+		// } else {
+		// // Angle needs to be adjusted
+		// return false;
+		// }
+
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.chassis.stopDriving();
+	}
+
+	// Called when another command which requires one or more of the same 
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		Robot.chassis.stopDriving();
+	}
 }
