d13227da0017d3c467db31b60b9d518b23ea0856
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2023-11-24 13:41:37 -0600

    Update logging and status frames

--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import org.littletonrobotics.junction.AutoLogOutput;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -29,8 +30,8 @@ public class Chassis extends SubsystemBase {
     private final MotorControllerGroup left, right;
     private final DifferentialDrive drive;
 
-    @AutoLogOutput
-    private ChassisSpeeds targetSpeeds = new ChassisSpeeds();
+    @AutoLogOutput(key = "Chassis/Target")
+    private ChassisSpeeds targetSpeeds;
 
     public Chassis() {
         frontLeft = new WPI_TalonSRX(Constants.FRONT_LEFT_CAN_ID);
@@ -68,15 +69,58 @@ public class Chassis extends SubsystemBase {
         left = new MotorControllerGroup(frontLeft, rearLeft);
         right = new MotorControllerGroup(frontRight, rearRight);
         drive = new DifferentialDrive(left, right);
-    }
 
-    private void drive(ChassisSpeeds speeds) {
-        targetSpeeds = speeds;
+        // https://v5.docs.ctr-electronics.com/en/stable/ch18_CommonAPI.html#setting-status-frame-periods
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 20);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 1000);
+        frontLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 20);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 1000);
+        rearLeft.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 20);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 1000);
+        frontRight.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 20);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 20);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_3_Quadrature, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_4_AinTempVbat, 20);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_8_PulseWidth, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_12_Feedback1, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 1000);
+        rearRight.setStatusFramePeriod(StatusFrameEnhanced.Status_21_FeedbackIntegrated, 1000);
 
-        drive.arcadeDrive(speeds.vxMetersPerSecond, speeds.omegaRadiansPerSecond, false);
+        targetSpeeds = new ChassisSpeeds();
     }
 
     public Command defaultCommand(Supplier<ChassisSpeeds> supplier) {
-        return run(() -> drive(supplier.get()));
+        return run(() -> targetSpeeds = supplier.get());
+    }
+
+    @Override
+    public void periodic() {
+        drive.arcadeDrive(targetSpeeds.vxMetersPerSecond, targetSpeeds.omegaRadiansPerSecond, false);
     }
 }
